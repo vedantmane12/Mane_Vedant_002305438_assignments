e211f92c856f2bb8fce10d13eeeca7066788fb3e
Vedant <vedant@Vedants-MacBook-Air.local>
2024-09-16 17:07:13 -0400

    Assignment 01 Commit

---
+++ Assignment 1/src/model/Person.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vedant
+ */
+public class Person {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String dlnumber;
+    private String dlstate;
+    private String socialsecuritynumber;
+    private String addline1;
+    private String addline2;
+    private String city;
+    private String state;
+    private String zip;
+    private String telephone;
+    private String faxnumber;
+    private String courseid;
+    private String university;
+    private String studentid;
+    private String course;
+    private String gpa;
+    private String year;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDlnumber() {
+        return dlnumber;
+    }
+
+    public void setDlnumber(String dlnumber) {
+        this.dlnumber = dlnumber;
+    }
+
+    public String getDlstate() {
+        return dlstate;
+    }
+
+    public void setDlstate(String dlstate) {
+        this.dlstate = dlstate;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getAddline1() {
+        return addline1;
+    }
+
+    public void setAddline1(String addline1) {
+        this.addline1 = addline1;
+    }
+
+    public String getAddline2() {
+        return addline2;
+    }
+
+    public void setAddline2(String addline2) {
+        this.addline2 = addline2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+
+    public String getCourseid() {
+        return courseid;
+    }
+
+    public void setCourseid(String courseid) {
+        this.courseid = courseid;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(String studentid) {
+        this.studentid = studentid;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(String gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+
+}
